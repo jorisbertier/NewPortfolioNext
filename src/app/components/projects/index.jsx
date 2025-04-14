e0d7f52af6149658c7b64ed4ac0bd7b9fb5ba78d
Joris Bertier <joris.bertier@gmail.com>
2025-04-14 09:58:48 -0500

    change

--- src/app/components/projects/index.jsx
+++ src/app/components/projects/index.jsx
@@ -7,7 +7,7 @@ import { ProjectsData } from '@/app/utils/ProjectsData';
 export default function Projects() {
     const [selectedProject, setSelectedProject] = useState(null)
     return (
-        <div className="relative mt-18 s:mt-0 z-30 w-full">
+        <div className="relative mt-14 s:mt-0 z-30 w-full">
             <Titles data={ProjectsData} setSelectedProject={setSelectedProject}/>
             <Descriptions data={ProjectsData} selectedProject={selectedProject}/>
         </div>
