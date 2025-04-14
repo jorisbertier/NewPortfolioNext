8fa2ee77c9caf24b99edf55821c39bd74a491004
Joris Bertier <joris.bertier@gmail.com>
2025-04-14 10:07:02 -0500

    change

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { useEffect, useState } from 'react';  
+import { useState } from 'react';  
 import { motion } from 'framer-motion';
 import useMousePosition from '../utils/useMousePosition';
 import StackSection from '../section/StackSection';
@@ -12,25 +12,6 @@ export default function HeroSection() {
     const [isHovered, setIsHovered] = useState(false);
     const { x, y } = useMousePosition();
     const size = isHovered ? 300 : 40;
-    const [ firstBottom, setFirstBottom] = useState(0)
-    const [ secondBottom, setSecondBottom] = useState(0)
-
-
-    useEffect(() => {
-        const handleResize = () => {
-          if (window.innerWidth < 450) {
-            setFirstBottom(80);
-            setSecondBottom(80);
-          } else {
-            setFirstBottom(45);
-            setSecondBottom(90);
-          }
-        };
-    
-        handleResize();
-        window.addEventListener('resize', handleResize);
-        return () => window.removeEventListener('resize', handleResize);
-      }, []);
 
     return (
         <main className="relative h-[310vh] ss:h-[400vh] w-full overflow-x-hidden overflow-y-hidden flex flex-col text-brown text-4xl leading-[66px] cursor-default max-w-full ">
@@ -40,11 +21,11 @@ export default function HeroSection() {
                 <p className="md:leading-[100px] sm:leading-[70px] max-w-[1000px] uppercase p-10 text-center font-avantGarde text-[3rem] xs:text-[3.6rem] s:text-[4rem] md:text-[6rem] font-bold">
                     I develop<br></br> <span className="text-primary">{'things'} </span><br></br> for <br></br>the web
                 </p>
-                <PathDrawing bottom={firstBottom}/>
-                <PathDrawing bottom={secondBottom}/>
+                <PathDrawing bottom={45}/>
+                <PathDrawing bottom={90}/>
             </div>
             {/* <StackSection/> */}
-            <div className='z-30 mt-8 ss:mt-14 s:mt-0 p-0' id="work">
+            <div className='z-30 mt-4 ss:mt-14 s:mt-0 p-0' id="work">
                 <RevealName name={'Works'}/>
                 <Projects/>
             </div>
