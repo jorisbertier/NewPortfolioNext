7fbb5b00c936305f622c191fa935f4362195dcde
Joris Bertier <joris.bertier@gmail.com>
2025-04-14 11:10:59 -0500

    landscape

--- src/app/guard/OrientationGuard.jsx
+++ src/app/guard/OrientationGuard.jsx
@@ -4,26 +4,26 @@ import { useEffect, useState } from "react";
 
 export default function OrientationGuard({ children }) {
     const [orientation, setOrientation] = useState("");
-    const isSmallScreen = window.innerWidth < 450;
 
-    useEffect(() => {
-        function updateOrientation() {
-            if (window.innerWidth < 450) {
-                setOrientation(window.screen.orientation.type);
-            } else {
-                setOrientation("");
-            }
+useEffect(() => {
+    function updateOrientation() {
+        if (window.innerWidth < 450) {
+            setOrientation(window.screen.orientation.type);
+        } else {
+            setOrientation(""); // reset si écran trop large
         }
-    
-        updateOrientation();
-        window.addEventListener("orientationchange", updateOrientation);
-        window.addEventListener("resize", updateOrientation);
-    
-        return () => {
-            window.removeEventListener("orientationchange", updateOrientation);
-            window.removeEventListener("resize", updateOrientation);
-        };
-    }, [isSmallScreen]);
+    }
+
+    updateOrientation();
+    window.addEventListener("orientationchange", updateOrientation);
+    window.addEventListener("resize", updateOrientation); // pour gérer aussi resize
+
+    return () => {
+        window.removeEventListener("orientationchange", updateOrientation);
+        window.removeEventListener("resize", updateOrientation);
+    };
+}, [orientation]);
+
     if (orientation === "landscape-primary") {
         return (
             <div className="flex items-center justify-center min-h-screen bg-[#171717]">
