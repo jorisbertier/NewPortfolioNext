634225445374a8479b76abce6f1aa533962676ac
Joris Bertier <joris.bertier@gmail.com>
2025-04-14 11:32:13 -0500

    change

--- src/app/components/HeroSection.tsx
+++ src/app/components/HeroSection.tsx
@@ -25,7 +25,7 @@ export default function HeroSection() {
                 <PathDrawing bottom={90}/>
             </div>
             {/* <StackSection/> */}
-            <div className='z-30 mt-20 ss:mt-14 s:mt-0 p-0' id="work">
+            <div className='z-30 mt-20 ss:mt-14 p-0' id="work">
                 <RevealName name={'Works'}/>
                 <Projects/>
             </div>
